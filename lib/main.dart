56f229e0030b5d6c44915a1e537477e5a9cd93d6
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-11-07 08:10:10 +0700

    upload main.dart baru

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './screen/splash_screen_1.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber)
+      ),
+      home: SplashScreen1(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
